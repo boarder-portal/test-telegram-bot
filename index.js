6dff4ab842709e3da23b8c5886de44d0a1c8948e
droooney <d.vor7@mail.ru>
2018-04-03 20:21:42 +0500

    Init bot.

---
+++ index.js
@@ -0,0 +1,36 @@
+const Application = require('koa');
+const bodyParser = require('koa-bodyparser');
+const axios = require('axios');
+
+const {
+  PORT = 3001,
+  TELEGRAM_BOT_ID
+} = process.env;
+
+console.log(`State up: ${new Date().toJSON()}`);
+
+const app = new Application();
+
+app
+  .use(bodyParser())
+  .use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      ctx.body = '';
+    }
+  })
+  .use(async (ctx, next) => {
+    if (ctx.url !== `/new-message/${TELEGRAM_BOT_ID}` || ctx.method !== 'POST') {
+      return next();
+    }
+
+    console.log(ctx.request.body);
+
+    await next();
+  })
+  .listen(PORT, () => {
+    console.log(`Telegram app listening on port ${PORT}!`);
+  });
